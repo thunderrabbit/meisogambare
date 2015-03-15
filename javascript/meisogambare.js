166e57ed4828fccea75eaf5995d5ac58abc34f70
Rob Nugen <rob@robnugen.com>
2015-03-15 12:03:23 +0900

    Move JS to its own file

---
+++ javascript/meisogambare.js
@@ -0,0 +1,89 @@
+var clock;
+var reveal_duration = 2000;
+var hide_duration = 2000;
+var successBGColor = "#0B6138";
+var countingColor = "#232323";
+var reachedGoalTime = false;
+
+var clickedStartButton = function(e) {
+	reachedGoalTime = false;
+	changePageColor(countingColor);
+	clock.setTime($('#countdown_minutes').val() * 60);
+	clock.setCountdown(true);
+	clock.start();
+	hideStuffs();
+}
+
+var clickedStopButton = function(e) {
+	clock.stop();
+	setSuccessString();
+	revealStuffs();
+}
+
+var hideStuffs = function() {
+	$('.start').hide(hide_duration);
+	$('.duration-field-wrapper').hide(hide_duration);
+	$('.share').hide(reveal_duration);
+}
+
+var revealStopButton = function() {
+	$('.stop').show(reveal_duration);
+}
+
+var countDownFinished = function() {
+	document.getElementById("audio-bell").play();
+	revealStopButton();
+	changePageColor(successBGColor);
+}
+
+var revealStuffs = function() {
+	$('.stop').hide(reveal_duration);
+	$('.share').show(reveal_duration);
+	$('.start').show(hide_duration);
+	$('.duration-field-wrapper').show(hide_duration);
+
+}
+
+var setSuccessString = function() {
+	var intialMinutes = $('#countdown_minutes').val();
+	var extraSeconds = clock.getTime().time;
+	var extraMinutes = Math.floor(extraSeconds / 60);
+	var successString;
+
+	if(extraMinutes) {
+		successString = "I meditated for " + intialMinutes + " minutes and " + extraMinutes + " bonus minutes!";
+	} else {
+		successString = "I meditated for " + intialMinutes + " minutes!";
+	}
+	// putting string here just for convenience
+	// we could unhide this field or send its value to anything we like
+	$("#share_success_string").val(successString);
+	$("#twitter_link").attr("href","http://twitter.com/share/?text=" + successString);
+}
+
+var changePageColor = function(newColor) {
+	$('.body').css({backgroundColor:newColor},1000);
+}
+
+$(document).ready(function() {
+	clock = $('.clock').FlipClock({
+		clockFace: 'MinuteCounter',
+		countdown: true,
+		autoStart: false,
+		callbacks: {
+			interval: function() {
+				if(clock.getTime().time == 0 && !reachedGoalTime) {
+					reachedGoalTime = true;
+					clock.setCountdown(false);
+					clock.start();
+					countDownFinished();
+				}
+			}
+		}
+	});
+	clock.setTime($('#countdown_minutes').val() * 60);
+
+	$('.start').click(clickedStartButton);
+
+	$('.stop').click(clickedStopButton)
+});
